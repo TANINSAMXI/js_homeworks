39b9eea36400242eea3b4c1f6eacfa18ad2c8cb5
TANINSAMXI <pischaniyd@gmail.com>
2024-08-01 19:30:21 +0200

    hw_3 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+const firstNumber = Number(prompt("please enter the first number:"));
+const secondNumber = Number(prompt("please enter the second number:"));
+
+//finding the sum
+const addition = firstNumber + secondNumber;
+console.log(`result: ${firstNumber} + ${secondNumber} = ${addition}`);
+
+//finding the difference
+const subtraction = firstNumber - secondNumber;
+console.log(`result: ${firstNumber} - ${secondNumber} = ${subtraction}`);
+
+//finding the product
+const multiplication = firstNumber * secondNumber;
+console.log(`result: ${firstNumber} * ${secondNumber} = ${multiplication}`);
+
+//finding the quotient
+if (secondNumber !== 0) {
+    const division = firstNumber / secondNumber;
+    console.log(`result: ${firstNumber} / ${secondNumber} = ${division}`);
+}
+else {
+    console.log("error: cannot divide by zero!");
+}
