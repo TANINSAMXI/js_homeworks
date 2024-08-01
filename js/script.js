bc7db5889593a62db6fa196091199aa64943b335
TANINSAMXI <pischaniyd@gmail.com>
2024-08-01 18:30:45 +0200

    hw_2 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// #1
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+
+// #2
+
+let planetName;
+planetName = 'Earth';
+let userName;
+userName = 'John';
+
+// #3
+
+const stringPlusNumber = '10' + 10;
+console.log(stringPlusNumber );
+console.log(typeof stringPlusNumber);
