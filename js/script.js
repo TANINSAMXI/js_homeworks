3c82178df144a0eb5367b24107e99001597b2b02
TANINSAMXI <pischaniyd@gmail.com>
2024-08-08 17:30:40 +0200

    hw_7 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const hours = +prompt("Please enter the number of hours:");
+
+if (isNaN(hours) || hours < 0 ) {
+
+    alert("Please enter the correct number of hours!");
+}
+
+else {
+
+    const sec = hours * 3600;
+
+    alert( sec + ' seconds' );
+}
