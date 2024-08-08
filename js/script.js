c56fc3603978c97d57b8438fbf1ad1aa557a88f2
TANINSAMXI <pischaniyd@gmail.com>
2024-08-08 17:56:52 +0200

    hw_8 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const firstNumb = +prompt("Please enter first number:");
+const secondNumb = +prompt("Please enter second number:");
+const thirdNumb = +prompt("Please enter third number:");
+
+if (isNaN(firstNumb) || isNaN(secondNumb) || isNaN(thirdNumb)) {
+
+    alert("Warning ! Enter numbers only !");
+
+}
+
+else {
+
+    const result = (firstNumb + secondNumb + thirdNumb) / 3;
+
+    alert('Your result : ' + result);
+}
