22ad78ea10435aeace5a69cb3f4eb2de91081cb7
TANINSAMXI <pischaniyd@gmail.com>
2024-08-22 22:48:53 +0200

    hw_15 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const generateKey = function (length, characters) {
+    let span = ''
+    for (let i = 0; i < length; i++) {
+        span = span + characters[Math.floor(Math.random() * characters.length)]
+    }
+    return span
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+const key = generateKey(16, characters)
+console.log(key)
