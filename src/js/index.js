89e4e130f50a275f37657e2e022a6dd9eeb51df2
TANINSAMXI <pischaniyd@gmail.com>
2024-08-23 18:55:35 +0200

    hw_16 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const padString = function (string, num, symbol = ' ', rightSide = true) {
+    if (typeof string !== 'string') {
+        return 'Error!'
+    }
+    if (typeof num !== 'number') {
+        return 'Error!'
+    }
+    if (typeof symbol !== 'string' || symbol.length !== 1) {
+        return 'Error!'
+    }
+    if (typeof rightSide !== 'boolean') {
+        return 'Error!'
+    }
+    if (string.length >= num) {
+        return string.substring(0, num)
+    }
+    const size = num - string.length
+    const count = symbol.repeat(size)
+
+    if (rightSide) {
+        return string + count
+    } else {
+        return count + string
+    }
+}
+console.log(padString('hello', 8, '*'))
+console.log(padString('hello', 6, '*', false))
+console.log(padString('hello', 2))
+console.log(padString('hello', 25, 'o', true))
