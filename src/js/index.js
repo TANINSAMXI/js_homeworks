251c9d97d34762b51fc4089379cb86252f392464
TANINSAMXI <pischaniyd@gmail.com>
2024-08-28 19:26:57 +0200

    hw_17 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const removeElement = function (array, item) {
+    const el = array.indexOf(item)
+    array.splice(el, 1)
+    return array
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7]
+removeElement(array, 5)
+console.log(array)
