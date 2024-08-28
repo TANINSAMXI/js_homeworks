0d497797f4505dd96e77597c49939f63d2a4af6f
TANINSAMXI <pischaniyd@gmail.com>
2024-08-28 21:59:00 +0200

    hw_18 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+//#1
+
+const onlyNumbers = function (array) {
+    let summa = 0
+    let divider = 0
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'number') {
+            summa = summa + array[i]
+            divider = divider + 1
+        }
+    }
+    return summa / divider
+}
+const myArray = onlyNumbers([20, 'some', 20, 40, null, 15, 15, '100'])
+console.log(myArray)
