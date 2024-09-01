cc2d0e000a79392fd5d303a422d567a75d41163e
TANINSAMXI <pischaniyd@gmail.com>
2024-09-01 11:28:35 +0200

    hw_19 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,40 @@
 'use strict'
+
+//Main
+const mainFunction = function (callback) {
+    const firstNumber = +prompt('please enter your first number')
+    const secondNumber = +prompt('please enter your second number')
+    callback(firstNumber, secondNumber)
+}
+
+//Callback exponentiation
+const exponentiation = function (num1, num2) {
+    const answer = num1 ** num2
+    alert(`${num1} ^ ${num2} = ${answer}`)
+    console.log(`${num1} ^ ${num2} = ${answer}`)
+}
+mainFunction(exponentiation)
+
+//Callback multiply
+const multiply = function (num1, num2) {
+    const answer = num1 * num2
+    alert(`${num1} * ${num2} = ${answer}`)
+    console.log(`${num1} * ${num2} = ${answer}`)
+}
+mainFunction(multiply)
+
+//Callback division
+const division = function (num1, num2) {
+    const answer = num1 / num2
+    alert(`${num1} / ${num2} = ${answer}`)
+    console.log(`${num1} / ${num2} = ${answer}`)
+}
+mainFunction(division)
+
+//Callback modulo
+const modulo = function (num1, num2) {
+    const answer = num1 % num2
+    alert(`${num1} % ${num2} = ${answer}`)
+    console.log(`${num1} % ${num2} = ${answer}`)
+}
+mainFunction(modulo)
