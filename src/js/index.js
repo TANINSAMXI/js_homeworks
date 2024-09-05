b92602043662fc202251e941e5f6c5a0259d5da0
TANINSAMXI <pischaniyd@gmail.com>
2024-09-05 19:51:34 +0200

    hw_21 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const yourNumbers = () => {
+    const length = +prompt('enter number of elements of your array')
+    const arr = []
+    for (let i = 0; i < length; i++) {
+        arr.push(+prompt(`enter some number for your array `))
+    }
+    console.log(arr)
+    return arr
+}
+const yourArr = yourNumbers()
+
+const increasing = (arr) => {
+    arr.sort((a, b) => a - b)
+    console.log(arr)
+    return arr
+}
+const newArr = increasing(yourArr)
+
+const deleteNumbers = (arr) => {
+    arr.splice(1, 3)
+    console.log(arr)
+    return arr
+}
+deleteNumbers(newArr)
