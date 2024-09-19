fcc078c93b7fbdb5750734e80c4229e31ff7bb40
TANINSAMXI <pischaniyd@gmail.com>
2024-09-19 19:35:59 +0200

    hw_27 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 'use strict'
+
+;(() => {
+    const myPet = {
+        name: 'Marko',
+        animal: 'cat',
+    }
+    const returnString = (contextS = this, argumentS = '') => {
+        return argumentS + contextS
+    }
+    console.log(returnString(myPet.animal, 'my pet is  '))
+    alert(returnString(myPet.animal, 'my pet is  '))
+    const bindBind = (contextS = this, argumentS = '') => {
+        return () => {
+            return returnString(contextS, argumentS)
+        }
+    }
+    console.log(bindBind(myPet.name, 'his name is  ')())
+    alert(bindBind(myPet.name, 'his name is  ')())
+})()
