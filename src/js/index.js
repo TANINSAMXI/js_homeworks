ab2f0cc2fd79e1be6e4f4dc7dc6bdf13ea06f237
TANINSAMXI <pischaniyd@gmail.com>
2024-09-19 20:56:19 +0200

    hw_28 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,45 @@
 'use strict'
+
+//#1
+const getFactorial = (num) => {
+    let ourFactorial = 1
+    const multipliers = []
+    for (let i = 1; i <= num; i++) {
+        ourFactorial *= i
+        multipliers.push(i)
+    }
+    const operations = `${num}! = ${multipliers.reverse().join(' * ')} = `
+    const end1 = operations + ourFactorial
+    console.log(end1)
+    alert(end1)
+    return ourFactorial
+}
+getFactorial(3)
+
+//#2
+const pow = (num, degree) => {
+    if (degree === 0) {
+        return 1
+    }
+    return num * pow(num, degree - 1)
+}
+const outPut = (num, degree) => {
+    const recursive = pow(num, degree)
+    const end2 = `${num} ^ ${degree} = ${recursive}`
+    console.log(end2)
+    alert(end2)
+}
+outPut(4, 3)
+
+//#3
+
+const increment = (a, b) => {
+    if (b === 0) {
+        return a
+    }
+    return increment(a + 1, b - 1)
+}
+const pseudoAddition = (a, b) => {
+    return increment(a, b)
+}
+console.log(pseudoAddition(100, 117))
