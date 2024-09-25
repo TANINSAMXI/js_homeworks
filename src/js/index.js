5db382967ecf7309ac7a79aa7f9cc0dcf7a65e10
TANINSAMXI <pischaniyd@gmail.com>
2024-09-25 20:33:17 +0200

    hw_33 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const nineJpg = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+]
+const someOfNine = () => {
+    document.getElementById('img').src =
+        `images/${nineJpg[~~(Math.random() * nineJpg.length)]}`
+}
+someOfNine()
