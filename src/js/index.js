c4d6d2bf3006fcac253b4a6e54dbb7c3d36c64ba
TANINSAMXI <pischaniyd@gmail.com>
2024-09-26 21:10:33 +0200

    hw_32  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const oneHundredTable = () => {
+    let table = '<table>'
+
+    for (let i = 0; i < 100; i++) {
+        if (i % 10 === 0) {
+            table = table + '<tr>'
+        }
+        table = table + '<td>' + (i + 1) + '</td>'
+        if (i % 10 === 9) {
+            table += '</tr>'
+        }
+    }
+    table += '</table>'
+    document.body.innerHTML = table
+}
+oneHundredTable()
