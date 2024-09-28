8e32d4e332fb4360479238b5c9c29326be7a94c1
TANINSAMXI <pischaniyd@gmail.com>
2024-09-28 20:39:22 +0200

    hw_34  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const myText = document.getElementById('textField')
+const myDiv = document.getElementById('appear')
+
+myDiv.style.display = 'none'
+myText.addEventListener('focus', () => {
+    myDiv.style.display = 'block'
+})
+
+myText.addEventListener('blur', () => {
+    myDiv.style.display = 'none'
+})
