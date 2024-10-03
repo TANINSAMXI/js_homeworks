31750181aa753b32e56be3fa2679efd9ed365665
TANINSAMXI <pischaniyd@gmail.com>
2024-10-03 18:19:39 +0200

    hw_32  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,46 @@
 'use strict'
 
-let number = 1
-const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
-    if (!parentEl || !elementToAppend) return
-    parentEl.append(elementToAppend)
-}
-
-const generateTable = () => {
-    const tableSize = 10
+const generateTable = (rows, columns) => {
+    if (
+        typeof rows !== 'number' ||
+        typeof columns !== 'number' ||
+        rows <= 0 ||
+        columns <= 0
+    ) {
+        console.error('arguments can be  only positive numbers!')
+        return null
+    }
     const table = document.createElement('table')
     const tbody = document.createElement('tbody')
-    table.append(tbody)
-    for (let i = 0; i < tableSize; i++) {
+    table.appendChild(tbody)
+    let cellContent = createCellCounter()
+    for (let i = 0; i < rows; i++) {
         const tr = document.createElement('tr')
-        for (let c = 0; c < tableSize; c++) {
+        for (let c = 0; c < columns; c++) {
             const td = document.createElement('td')
-            td.innerText = `${number}`
-            number++
-            tr.append(td)
+            td.innerText = cellContent()
+            tr.appendChild(td)
         }
-        tbody.append(tr)
+        tbody.appendChild(tr)
     }
     return table
 }
+const appendHTMLElement = (parentElement, elementToAppend) => {
+    if (
+        !(parentElement instanceof HTMLElement) ||
+        !(elementToAppend instanceof HTMLElement)
+    ) {
+        console.error('arguments can be only HTML elements!')
+        return
+    }
+    parentElement.appendChild(elementToAppend)
+}
+const createCellCounter = () => {
+    let count = 1
+    return () => count++
+}
+const tableElement = generateTable(10, 10)
 
-appendHTMLElement(document.body, generateTable())
-
-// const oneHundredTable = () => {
-//     let table = '<table>'
-//
-//     for (let i = 0; i < 100; i++) {
-//         if (i % 10 === 0) {
-//             table = table + '<tr>'
-//         }
-//         table = table + '<td>' + (i + 1) + '</td>'
-//         if (i % 10 === 9) {
-//             table += '</tr>'
-//         }
-//     }
-//     table = table + '</table>'
-//     document.body.innerHTML = table
-// }
-// oneHundredTable()
+if (tableElement !== null) {
+    appendHTMLElement(document.body, tableElement)
+}
