29cd5c69674394174485a0177fc836ff639a8444
TANINSAMXI <pischaniyd@gmail.com>
2024-10-05 17:10:36 +0200

    hw_34  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,34 @@
 'use strict'
 
-const inputText = document.getElementById('textField')
-const displayDiv = document.getElementById('appear')
-displayDiv.style.display = 'none'
-document.addEventListener('focusin', () => {
-    if (document.activeElement === inputText) {
-        displayDiv.style.display = 'flex'
-    }
-})
-document.addEventListener('focusout', () => {
-    if (document.activeElement !== inputText) {
+const creatingElements = () => {
+    const inputText = document.createElement('input')
+    inputText.id = 'textField'
+    inputText.type = 'text'
+    inputText.placeholder = 'click here to focus on me!'
+
+    const displayDiv = document.createElement('div')
+    displayDiv.id = 'appear'
+    displayDiv.style.display = 'none'
+
+    const img = document.createElement('img')
+    img.src = 'https://m.media-amazon.com/images/I/81xteigXbwL._AC_SX679_.jpg'
+    img.alt = 'image_link'
+    displayDiv.appendChild(img)
+
+    document.body.appendChild(inputText)
+    document.body.appendChild(displayDiv)
+}
+
+const addEventListeners = (inputText, displayDiv) => {
+    inputText.addEventListener('focusin', () => {
+        displayDiv.style.display = 'block'
+    })
+    inputText.addEventListener('focusout', () => {
         displayDiv.style.display = 'none'
-    }
-})
+    })
+}
+
+creatingElements()
+const getInputText = document.getElementById('textField')
+const getDisplayDiv = document.getElementById('appear')
+addEventListeners(getInputText, getDisplayDiv)
