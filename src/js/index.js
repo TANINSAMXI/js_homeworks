b4a1c5ace3774980bb0a704a7a93de7993b486cf
TANINSAMXI <pischaniyd@gmail.com>
2024-10-06 13:17:59 +0200

    hw_36  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 'use strict'
+;(function () {
+    const createUl = (array) => {
+        const ul = document.createElement('ul')
+        array.forEach((item) => {
+            const li = document.createElement('li')
+            li.textContent = item
+            if (Array.isArray(item)) {
+                li.append(createUl(item))
+            }
+            ul.append(li)
+        })
+        return ul
+    }
+
+    const arrOfNum = [1, 2, 3]
+    document.body.append(createUl(arrOfNum))
+
+    const arrOfArrays = [1, 2, [1.1, 1.2, 1.3], 3]
+    document.body.append(createUl(arrOfArrays))
+})()
