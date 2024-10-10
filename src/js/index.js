11515e8897d1a714c5a0a56a912c50ed7f9a9cab
TANINSAMXI <pischaniyd@gmail.com>
2024-10-10 18:44:35 +0200

    hw_36  complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,45 @@
 'use strict'
 ;(function () {
-    const createUl = (array) => {
-        const ul = document.createElement('ul')
-        array.forEach((item) => {
-            const li = document.createElement('li')
-            li.textContent = item
-            if (Array.isArray(item)) {
-                li.append(createUl(item))
-            }
-            ul.append(li)
-        })
-        return ul
+    const LIST_TYPES = {
+        UL: 'ul',
+        OL: 'ol',
+        MENU: 'menu',
     }
 
-    const arrOfNum = [1, 2, 3]
-    document.body.append(createUl(arrOfNum))
+    const appendElement = (parentEl = null, elementToAppend = null) => {
+        if (!parentEl || !elementToAppend) return
+        parentEl.append(elementToAppend)
+    }
 
-    const arrOfArrays = [1, 2, [1.1, 1.2, 1.3], 3]
-    document.body.append(createUl(arrOfArrays))
+    const createList = (listItems, listType = LIST_TYPES.UL) => {
+        if (!Object.values(LIST_TYPES).includes(listType)) {
+            throw new Error('Invalid list type')
+        }
+        if (!Array.isArray(listItems)) {
+            throw new Error('listItems must be an array')
+        }
+        if (listItems.length === 0) {
+            throw new Error('listItems array cannot be empty')
+        }
+
+        const listElement = document.createElement(listType)
+
+        listItems.forEach((item) => {
+            const listItemElement = document.createElement('li')
+            appendElement(listElement, listItemElement)
+
+            if (Array.isArray(item)) {
+                const nestedList = createList(item, listType)
+                appendElement(listItemElement, nestedList)
+            } else {
+                listItemElement.textContent = item
+            }
+            appendElement(listElement, listItemElement)
+        })
+
+        return listElement
+    }
+
+    const resultList = createList([1, 2, 3, [1, 2, 3]], 'ol')
+    appendElement(document.body, resultList)
 })()
