5f97058a452849cd5f545d8a68308eb6bb9d5d34
TANINSAMXI <pischaniyd@gmail.com>
2024-10-24 19:34:52 +0200

    hw_41 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,57 @@
 'use strict'
+;(function () {
+    const product = {}
+
+    Object.defineProperty(product, 'name', {
+        get() {
+            return this._name //
+        },
+        set(value) {
+            if (typeof value === 'string' && value.trim() !== '') {
+                this._name = value
+            } else {
+                throw new Error(' empty string !')
+            }
+        },
+        enumerable: true,
+        configurable: true,
+    })
+
+    Object.defineProperty(product, 'price', {
+        get() {
+            return this._price
+        },
+        set(value) {
+            if (typeof value === 'number' && value > 0) {
+                this._price = value
+            } else {
+                throw new Error('price must be bigger than 0 !')
+            }
+        },
+        enumerable: true,
+        configurable: true,
+    })
+
+    Object.defineProperty(product, 'quantity', {
+        value: 2,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+    })
+
+    Object.defineProperty(product, 'totalValue', {
+        get() {
+            return this.price * this.quantity
+        },
+        enumerable: true,
+        configurable: true,
+    })
+
+    product.name = 'JS'
+    product.price = 10
+
+    console.log(product.name)
+    console.log(product.price)
+    console.log(product.quantity)
+    console.log(product.totalValue)
+})()
